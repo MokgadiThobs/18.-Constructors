c005dadc0aef4d01b6ab5720c562c1c4a0e4bee1
Dell-User <Dell-User@DESKTOP-8EU8MHV>
2023-10-11 03:05:25 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Constructors
+{
+    class Book
+    {
+
+        public string title;
+        public string author;
+        public int pages;
+
+        //this below is called a constructor
+        public Book(string aTitle, string aAuthor,int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+    }
+}
